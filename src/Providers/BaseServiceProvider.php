944ad26f0afdd67a2475274c523a2f6d4a9be6d9
Daniel Elskamp <de@3b.de>
2024-01-03 16:40:30 +0100

    feat: adding provider

---
+++ src/Providers/BaseServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Schaefersoft\Base\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+    }
+}
