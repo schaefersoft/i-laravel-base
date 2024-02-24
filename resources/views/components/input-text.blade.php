85756339b1cc72730edbe0aa14118eb2cfe27e79
Daniel Elskamp <de@3b.de>
2024-02-24 17:23:25 +0100

    feat: adding error validation message

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="relative mb-2">
-        <label class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900" for="{{$name}}">
+        <label class="absolute -top-2 left-2 inline-block bg-white dark:bg-gray-900 px-1 text-xs font-medium text-gray-900 dark:text-white" for="{{$name}}">
             {{ $label }}
         </label>
         <input
-            class="ring-gray-300 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-theme-600 sm:text-sm sm:leading-6'"
+            class="ring-gray-300  dark:bg-gray-900 text-black dark:text-white block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-theme-600 sm:text-sm sm:leading-6'"
             name="{{$name}}"
             id="{{$name}}"
             type="{{$type}}"
@@ -23,6 +23,9 @@
             @endif
             value="{{$value}}"
         />
+        @error($name)
+        <p class="text-sm text-red-600">{{ $message }}</p>
+        @enderror
     </div>
 </div>
 
