6ea1877918ac444fcbf2e1c34cbab307aee7bd17
Luca Schäfer <luca.schaefer1594@gmail.com>
2024-02-24 14:12:29 +0100

    fix(core): fix updated & created by trait

--- src/Traits/Models/CreatedUpdatedByTrait.php
+++ src/Traits/Models/CreatedUpdatedByTrait.php
@@ -14,17 +14,17 @@ trait CreatedUpdatedByTrait
 
         static::creating(function ($model) {
             if (!$model->isDirty('created_by')) {
-                $model->created_by = auth()->user()->id;
+                $model->created_by = auth()->user()->id ?? null;
             }
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = auth()->user()->id;
+                $model->updated_by = auth()->user()->id ?? null;
             }
         });
 
         // updating updated_by when model is updated
         static::updating(function ($model) {
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = auth()->user()->id;
+                $model->updated_by = auth()->user()->id ?? null;
             }
         });
     }
