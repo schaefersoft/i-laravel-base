b43541b28e9e5da8ac24153c1d82b5a85c82de72
Daniel Elskamp <de@3b.de>
2024-02-24 17:23:11 +0100

    feat: remove errors from blade

--- resources/views/components/errors.blade.php
+++ resources/views/components/errors.blade.php
@@ -1,4 +1,4 @@
-if(session()->get('warning'))
+@if(session()->get('warning'))
 <div class="border-l-4 border-yellow-400 bg-yellow-50 p-4">
     <div class="flex">
         <div class="flex-shrink-0">
@@ -44,24 +44,6 @@ if(session()->get('warning'))
                 <p class="text-sm text-red-700">
                     {{session()->get('error')}}
                 </p>
-            </div>
-        </div>
-    </div>
-@endif
-@if($errors->any())
-    <div class="border-l-4 border-red-400 bg-red-50 p-4">
-        <div class="flex">
-            <div class="flex-shrink-0">
-                <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-                </svg>
-            </div>
-            <div class="ml-3">
-                <ul class="text-sm text-red-700 list-none">
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
             </div>
         </div>
     </div>
