8b241e9477b28e7192aac3f9345fac2f49d6a4d1
Daniel Elskamp <de@3b.de>
2024-02-28 14:21:14 +0100

    feat: added error message component

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -146,6 +146,9 @@
             @endif
             <main class="py-10">
                 <div class="px-4 sm:px-6 lg:px-8">
+                    <div class="my-2">
+                        <x-base::errors />
+                    </div>
                     @yield('content')
                 </div>
             </main>
