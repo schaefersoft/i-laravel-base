b74635872b4bf4e4d6c56da7f113e1c2a2578d5a
Daniel Elskamp <de@3b.de>
2024-03-02 09:41:36 +0100

    fix: light mode fix for username

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -140,7 +140,7 @@
                                              alt="">
                                     @endif
                                     <span class="hidden lg:flex lg:items-center">
-                                        <span class="ml-4 text-sm font-semibold leading-6 text-white" aria-hidden="true">{{auth()->user()->$userMenuNameIndicator}}</span>
+                                        <span class="ml-4 text-sm font-semibold leading-6 dark:text-white" aria-hidden="true">{{auth()->user()->$userMenuNameIndicator}}</span>
                                         <svg class="ml-2 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd"
                                                 d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
